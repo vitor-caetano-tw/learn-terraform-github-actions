babf62d23ca1bec86cee321f24793c53f6b5f173
Vitor Caetano <vitor.caetano@thoughtworks.com>
2022-12-12 18:42:38 -0300

    delete vpc

--- main.tf
+++ main.tf
@@ -28,9 +28,9 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
+# resource "google_compute_network" "vpc_network" {
+#   name = "terraform-network"
+# }
 
 # resource "google_compute_instance" "vm_instance" {
 #   name         = "terraform-instance"
